991dcd5f6726c0d146742ce7642bd8a976033e78
User2 <vkagarmanova@inbox.ru>
2025-04-14 17:27:50 +0500

    первый коммит: добавлен README

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Post> posts = new ArrayList<>();
+
+        for (int i = 1; i <= 1_000_000; i++) {
+            int randomLikes = (int)(Math.random() * 101);
+            String randomId = UUID.randomUUID().toString();
+            posts.add(new Post(randomId, randomLikes));
+        }
+        printList(getTop10(posts));
+    }
+
+     static List<Post> getTop10(List<Post> posts){
+        PriorityQueue<Post> postsQueue = new PriorityQueue<>(Comparator.comparing(Post::getLikesCount).reversed());
+
+        for (Post post : posts) {
+            if (postsQueue.size() < 10){
+                postsQueue.offer(post);
+            }
+            else {
+                if (post.getLikesCount() > postsQueue.peek().getLikesCount()){
+                    postsQueue.poll();
+                    postsQueue.offer(post);
+                }
+            }
+        }
+         return postsQueue.stream()
+                 .sorted(Comparator.comparing(Post::getLikesCount).reversed())
+                 .collect(Collectors.toList());
+    }
+
+    static void printList(List<Post> posts){
+        for (int i = 1; i <= posts.size(); i++){
+            System.out.println(i + ". " + posts.get(i-1).toString());
+        }
+    }
+}
